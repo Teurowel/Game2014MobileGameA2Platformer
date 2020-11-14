4084f9564373ba30983cd6c485b40bc0c56d1589
Teurowel <teurowel@gmail.com>
2020-11-14 13:34:46 +0900

    Added falling platform

---
+++ Assets/Script/Platform/FallingPlatform.cs
@@ -0,0 +1,68 @@
+﻿//SourceFileName : FallingPlatform.cs
+//Author's name : Doosung Jang
+//Studnet Number : 101175013
+//Date last Modified : Nov.14, 2020
+//Program description : This script handles falling platform
+//Revision History : Nov.14, 2020 Created, Added simple falling
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    Rigidbody2D rb = null;
+
+    [SerializeField] float fallingTime = 2.0f; //How much time does platform need to fall after player collided with it
+
+    Vector3 originPos = Vector3.zero; //Original position of platform, will be used to reset platform
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        originPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y <= -15.0f)
+        {
+            //Destroy(gameObject);
+            ResetPlatform();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //If collided with player, start coroutine that makes platform fall after designated seconds
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Invoke("StartFalling", fallingTime);
+        }
+    }
+
+    void StartFalling()
+    {
+        if(rb != null)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+    void ResetPlatform()
+    {
+        transform.position = originPos;
+
+        if(rb != null)
+        {
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            rb.velocity = new Vector3(0f, 0f, 0f);
+        }
+        
+    }
+}
---
+++ Assets/Script/Platform/RotatingPlatform.cs
@@ -0,0 +1,31 @@
+﻿//SourceFileName : RotatingPlatform.cs
+//Author's name : Doosung Jang
+//Studnet Number : 101175013
+//Date last Modified : Nov.14, 2020
+//Program description : This script handles rotating platform
+//Revision History : Nov.14, 2020 Created, Added simple rotating
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPlatform : MonoBehaviour
+{
+    [SerializeField] float speed = 2.0f; //rotation speed
+    [SerializeField] float rotatingDelay = 1.0f; //how much time does it need to rotate after rotate 180
+
+    float rotZ = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotZ += speed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotZ);
+    }
+}
