8f27d17c53ea09cf4becc0c002c168db82548da1
Teurowel <teurowel@gmail.com>
2020-11-15 13:10:59 +0900

    Added reset zone

---
+++ Assets/Script/ResetZone.cs
@@ -0,0 +1,38 @@
+﻿//SourceFileName : ResetZone.cs
+//Author's name : Doosung Jang
+//Studnet Number : 101175013
+//Date last Modified : Nov.15, 2020
+//Program description : This script handles reset zone that reset player to designated position when player falls.
+//Revision History : Nov.14, 2020 Created, Added OnTriggerEnter2D that if player enter this trigger, reset player
+
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetZone : MonoBehaviour
+{
+    [SerializeField] Transform resetPoint = null;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            if (resetPoint != null)
+            {
+                collision.gameObject.transform.position = resetPoint.position;
+            }
+        }
+    }
+}
