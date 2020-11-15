e21e244ddb81261f1274c3f39a70ce8b15bce4bb
Teurowel <teurowel@gmail.com>
2020-11-15 11:45:23 +0900

    Added goal Added SFX

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,34 @@
+﻿//SourceFileName : Goal.cs
+//Author's name : Doosung Jang
+//Studnet Number : 101175013
+//Date last Modified : Nov.14, 2020
+//Program description : This script handles goal
+//Revision History : Nov.14, 2020 Created, Added OnTriggerEnter2D that if player enter this trigger, game over
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GameOverScreen");
+        }
+    }
+}
