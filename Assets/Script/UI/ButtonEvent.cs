1487e72f1e00a96425fc10bd1d91d5313c3f8bca
Teurowel <teurowel@gmail.com>
2020-11-23 15:28:27 +0900

    Added playing button sfx in menu screen

---
+++ Assets/Script/UI/ButtonEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEvent : MonoBehaviour
+{
+    Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(PlayButtonSFX);
+        }
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    void PlayButtonSFX()
+    {
+        if (SoundManager.instance != null)
+        {
+            SoundManager.instance.Play("ButtonSFX");
+        }
+        else
+        {
+            Debug.Log("Should start from main menu scene since SoundManager is only created in menu scene");
+        }
+    }
+}
